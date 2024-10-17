637db4e897b180e62d3730b505d058af6131e939
jungyoonn <sophia7625@naver.com>
2024-10-17 20:35:49 +0900

    db 데이터 불러오기 쿼리 메서드 추가

---
+++ shop/src/main/java/com/anymall/common/DbCon.java
@@ -0,0 +1,39 @@
+package com.anymall.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbCon {
+    private static Connection con = null;
+    private static Statement stmt = null;
+    private static ResultSet rs = null;
+
+    public static void connectDatabase() throws SQLException {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try{
+            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "ANYMALL", "1234");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        stmt = con.createStatement();
+    }
+
+    public static void closeDatabase() throws SQLException {
+        rs.close();
+        stmt.close();
+        con.close();
+    }
+
+    public static ResultSet query(String query) throws SQLException{
+        rs = stmt.executeQuery(query);
+        return rs;
+    }
+}
