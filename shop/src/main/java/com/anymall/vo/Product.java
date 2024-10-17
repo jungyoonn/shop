2b27550aca274c92f92b357eeae07109500aac7c
jungyoonn <sophia7625@naver.com>
2024-10-17 17:59:39 +0900

    상품 vo

--- shop/src/main/java/com/anymall/vo/Product.java
+++ shop/src/main/java/com/anymall/vo/Product.java
@@ -1,5 +1,34 @@
 package com.anymall.vo;
 
+import java.sql.Date;
+
 public class Product {
-    
+    /**
+     * 상품 아이디
+     */
+    int id;
+    /**
+     * 상품 이름
+     */
+    String name;
+    /**
+     * 상품 가격
+     */
+    String price;
+    /**
+     * 제조사
+     */
+    String manufacturer;
+    /**
+     * 분류 아이디
+     */
+    int categoryId;
+    /**
+     * 사용 여부
+     */
+    boolean use;
+    /**
+     * 생성 날짜
+     */
+    Date createdDate;
 }
