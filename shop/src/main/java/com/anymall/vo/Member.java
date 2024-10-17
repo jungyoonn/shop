53106f431d3c6757e0f40678251b09d31edc1a11
jungyoonn <sophia7625@naver.com>
2024-10-17 17:58:43 +0900

    회원 vo

--- shop/src/main/java/com/anymall/vo/Member.java
+++ shop/src/main/java/com/anymall/vo/Member.java
@@ -6,12 +6,36 @@ import lombok.Data;
 
 @Data
 public class Member {
+    /**
+     * 회원 아이디
+    */
     String id;
+    /**
+     * 회원 비밀번호
+     */
     String password;
+    /**
+     * 회원 이름
+     */
     String name;
+    /** 
+     * 회원 생년월일
+     */
     Date birth;
+    /**
+     * 회원 성별
+     */
     boolean gender;
+    /**
+     * 회원 주소
+     */
     String address;
+    /**
+     * 회원 가입 날짜
+     */
     Date signDate;
+    /**
+     * 회원 등급
+     */
     String grade;
 }
