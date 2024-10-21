0c5563dd0be26b727f42736d5f437f7fe3bb44a6
kimyoungtae-kor <kyt4423@gmail.com>
2024-10-21 18:09:42 +0900

    카테고리 서비스 틀

---
+++ shop/src/main/java/com/anymall/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.anymall.service;
+
+import java.sql.SQLException;
+
+import com.anymall.dao.*;
+
+public class CategoryService {
+    private CategoryDao = categoryDao;
+
+    public void save() throws SQLException {
+
+    }
+
+    public void update() throws SQLException {
+
+    }
+
+    public void delete() throws SQLException {
+        
+    }
+}
