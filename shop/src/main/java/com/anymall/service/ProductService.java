d10d4c26fa132885c862c9f47ece10873fa0c65d
jungyoonn <sophia76256@gmail.com>
2024-10-21 18:20:49 +0900

    메서드 설명 주석 추가

--- shop/src/main/java/com/anymall/service/ProductService.java
+++ shop/src/main/java/com/anymall/service/ProductService.java
@@ -7,14 +7,23 @@ import com.anymall.dao.*;
 public class ProductService {
     private ProductDao productDao;
 
+    /**
+     * 상품 정보 저장 메서드
+     */
     public void save() throws SQLException {
         productDao.create();
     }
 
+    /**
+     * 상품 정보 변경 메서드
+     */
     public void update() throws SQLException {
         
     }
 
+    /**
+     *  상품 저장 메서드
+     */
     public void delete() throws SQLException {
         productDao.delete();
     }
